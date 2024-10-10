543039bf8fb75f24c32cfa03cd16f29de16bd76c
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-10 16:41:24 -0300

    feat: zoom algoritihm

--- test.c
+++ test.c
@@ -10,9 +10,6 @@ int main(){
     
     read_ppm("lena.ppm", image);
 
-    printf("Image width: %d \n", image->width);
-    printf("Image height: %d \n", image->height);
-
     int zoomWidth = image->width * 3;
     int zoomHeight = image->height * 3;
 
@@ -20,7 +17,7 @@ int main(){
 
     zoomed->width = zoomWidth;
     zoomed->height = zoomHeight;
-    printf("TESTE1 \n");
+
     populate_zoomed_image(image, zoomed);
 
     free_ppm(image);
@@ -30,14 +27,23 @@ int main(){
 };
 
 void populate_zoomed_image(struct image_s *image, struct image_s *zoomed){
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
+    for(int i = 0; i < image->height; i++){
+        for(int j = 0; j < image->width; j++){
             
             int old_image_position = i * image->width + j;
-            
-            for(int r = 0; r < 3; r++){
-                
-            }
+
+            int red = image->pix[old_image_position].r;
+            int green = image->pix[old_image_position].g;
+            int blue = image->pix[old_image_position].b;
+
+            int new_i = i * 3;
+            int new_j = j * 3;
+
+            fill_sp(zoomed, new_i, new_j, red, 0);
+
+            fill_sp(zoomed, new_i, new_j + 1, green, 1);
+
+            fill_sp(zoomed, new_i, new_j + 2, blue, 2);
 
         }
     }
@@ -46,11 +52,43 @@ void populate_zoomed_image(struct image_s *image, struct image_s *zoomed){
 }
 
 
-// void verify_color(int r, int g, int b){
-//     for(int m = 0; m < 3; m++){
-//         if(m == 0){
-           
-//         }
-//     }
-// }
-    
+void fill_sp(struct image_s *zoomed, int i, int j, int color, int color_channel) {
+    if (color <= 74) {
+        set_sub_pixel(zoomed, i, j, 0, 0, 0);
+        set_sub_pixel(zoomed, i + 1, j, 0, 0, 0);
+        set_sub_pixel(zoomed, i + 2, j, 0, 0, 0);
+    } else if (color <= 134) {
+        set_sub_pixel(zoomed, i, j, 0, 0, 0);
+        set_sub_pixel_with_channel(zoomed, i + 1, j, color, color_channel);
+        set_sub_pixel(zoomed, i + 2, j, 0, 0, 0);
+    } else if (color <= 179) {
+        set_sub_pixel_with_channel(zoomed, i, j, color, color_channel); 
+        set_sub_pixel(zoomed, i + 1, j, 0, 0, 0);
+        set_sub_pixel_with_channel(zoomed, i + 2, j, color, color_channel);
+    } else {
+        set_sub_pixel_with_channel(zoomed, i, j, color, color_channel);  
+        set_sub_pixel_with_channel(zoomed, i + 1, j, color, color_channel);
+        set_sub_pixel_with_channel(zoomed, i + 2, j, color, color_channel);
+    }
+}
+
+void set_sub_pixel_with_channel(struct image_s *zoomed, int i, int j, int color, int color_channel) {
+    int r = 0, g = 0, b = 0;
+
+    if (color_channel == 0) {
+        r = color;
+    } else if (color_channel == 1) {
+        g = color;
+    } else if (color_channel == 2) {
+        b = color;
+    }
+
+    set_sub_pixel(zoomed, i, j, r, g, b);
+}
+
+void set_sub_pixel(struct image_s *zoomed, int i, int j, int r, int g, int b) {
+    int position = i * zoomed->width + j;
+    zoomed->pix[position].r = r;
+    zoomed->pix[position].g = g;
+    zoomed->pix[position].b = b;
+}
--- zoom.h
+++ zoom.h
@@ -2,3 +2,9 @@
 
 
 void populate_zoomed_image(struct image_s *image, struct image_s *zoomed);
+
+void fill_sp(struct image_s *zoomed, int i, int j, int color, int color_channel);
+
+void set_sub_pixel(struct image_s *zoomed, int i, int j, int r, int g, int b);
+
+void set_sub_pixel_with_channel(struct image_s *zoomed, int i, int j, int color, int color_channel);
